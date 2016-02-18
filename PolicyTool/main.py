ab5d1fdb0662ade46ecab401fb132340f340b753
Trodis <galerian@gmx.net>
2016-02-18 06:22:55 +0100

    working!

---
+++ PolicyTool/main.py
@@ -0,0 +1,15 @@
+import sys
+from PySide import QtCore, QtGui
+from gui import Ui_MainWindow
+
+def main(argv):
+    app = QtGui.QApplication(argv)
+    mainWindow = QtGui.QMainWindow()
+
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main(sys.argv)
